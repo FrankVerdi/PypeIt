868a4fa32c9daff0bf4e3e4f899ff58a42ba656b
Brad Holden <brad.p.holden@gmail.com>
2025-06-25 11:41:17 -0700

    fix latest tweaks

--- pypeit/spectrographs/apf_levy.py
+++ pypeit/spectrographs/apf_levy.py
@@ -347,11 +347,9 @@ class APFLevySpectrograph(spectrograph.Spectrograph):
             return good_exp & (fitstbl['idname'] == 'Dark')
         if ftype in ['pixelflat']:
             return good_exp & (fitstbl['idname'] == 'WideFlat')
-        if ftype in ['illumflat']:
-            return good_exp & ((fitstbl['idname'] == 'NarrowFlat') & (fitstbl['decker'] == '3.0')) \
-                        | ((fitstbl['idname'] == 'WideFlat') & (fitstbl['decker'] == '8.0'))
         if ftype in ['trace']:
-            return good_exp & (fitstbl['idname'] == 'NarrowFlat') & (fitstbl['decker'] == '3.0')
+            return good_exp & (fitstbl['idname'] == 'NarrowFlat') & (fitstbl['decker'] == '3.0') \
+                        | (fitstbl['idname'] == 'WideFlat') & (fitstbl['decker'] == '8.0')
         if ftype in ['tilt']:
             return good_exp & (fitstbl['idname'] == 'ThAr') & \
                 ((fitstbl['decker'] == '8.0') | (fitstbl['decker'] == '3.0'))
