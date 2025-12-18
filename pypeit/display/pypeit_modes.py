c6dd541bf0b80c053b5d073868a2d00548381159
Eric Jeschke <eric@naoj.org>
2025-12-17 16:17:07 -1000

    Changes to track Ginga changes

--- pypeit/display/pypeit_modes.py
+++ pypeit/display/pypeit_modes.py
@@ -28,9 +28,9 @@ class Spec1DMode(Plot2DMode):
     * middle click : set pan position
     * scroll : zoom in/out
     * ctrl + scroll : zoom in/out X axis only
-    * shift + scroll : zoom in/out Y axis only
-    * alt + scroll : zoom in/out Y axis only
-    * meta + scroll : zoom in/out at cursor
+    * shift + scroll : zoom in/out Y axis only (On MacOS, trackpad only)
+    * alt + scroll(mouse) : zoom in/out Y axis only (Option key on Macs)
+    * alt + scroll : zoom in/out at cursor
 
     Keystroke bindings in mode
     --------------------------
@@ -39,19 +39,19 @@ class Spec1DMode(Plot2DMode):
     -------
     * equals : zoom in one zoom level
     * ctrl + equals : zoom in X axis one zoom level
-    * shift + equals (plus) : zoom in Y axis one zoom level
+    * plus (shift + equals) : zoom in Y axis one zoom level
     * minus : zoom out one zoom level
     * ctrl + minus : zoom out X axis one zoom level
-    * shift + minus (underscore): zoom out Y axis one zoom level
+    * underscore (shift + minus): zoom out Y axis one zoom level
     * 9 : zoom out maintaining cursor position
     * ctrl + 9 : zoom out X axis maintaining cursor position
-    * shift + 9 (left paren): zoom out Y axis maintaining cursor position
+    * left paren (shift + 9): zoom out Y axis maintaining cursor position
     * 0 : zoom in maintaining cursor position
     * ctrl + 0 : zoom in X axis maintaining cursor position
-    * shift + 0 (right paren): zoom in Y axis maintaining cursor position
-    * backquote : fit plot to window
-    * 1 : fit x axis only to window
-    * 2 : fit y axis only to window
+    * right paren (shift + 0): zoom in Y axis maintaining cursor position
+    * backquote : zoom X and Y axes to fit window
+    * 1 : zoom X axis only to fit window
+    * 2 : zoom Y axis only to fit window
     * k : set lower X range to X value at cursor
     * l : set upper X range to X value at cursor
     * K : set lower Y range to Y value at cursor
@@ -95,17 +95,29 @@ class Spec1DMode(Plot2DMode):
             ms_showxy=['spec1d+nobtn'],
             ms_panset2d=['spec1d+middle', 'spec1d+shift+left'],
 
-            sc_zoom2d=['spec1d+*+scroll'],
+            sc_zoom2d=['spec1d+scroll'],
+            sc_zoom2d_x=['spec1d+ctrl+scroll'],
+            sc_zoom2d_y=['spec1d+shift+scroll'],
+            sc_zoom2d_cursor=['spec1d+win+scroll'],
 
-            kp_pan_left=['spec1d+*+left'],
-            kp_pan_right=['spec1d+*+right'],
-            kp_pan_up=['spec1d+*+up'],
-            kp_pan_down=['spec1d+*+down'],
+            kp_pan_left=['spec1d+left'],
+            kp_pan_right=['spec1d+right'],
+            kp_pan_up=['spec1d+up'],
+            kp_pan_down=['spec1d+down'],
 
-            kp_zoom_in=['spec1d++', 'spec1d+='],
-            kp_zoom_out=['spec1d+-', 'spec1d+_'],
-            kp_zoom_cursor_in=['spec1d+*+0', 'spec1d+*+)'],
-            kp_zoom_cursor_out=['spec1d+*+9', 'spec1d+*+('],
+            kp_zoom_in=['spec1d+='],
+            kp_zoom_in_x=['spec1d+ctrl+='],
+            kp_zoom_in_y=['spec1d+shift++'],
+            kp_zoom_out=['spec1d+-'],
+            kp_zoom_out_x=['spec1d+ctrl+-'],
+            kp_zoom_out_y=['spec1d+shift+_'],
+
+            kp_zoom_cursor_in=['spec1d+0'],
+            kp_zoom_cursor_in_x=['spec1d+ctrl+0'],
+            kp_zoom_cursor_in_y=['spec1d+shift+)'],
+            kp_zoom_cursor_out=['spec1d+9'],
+            kp_zoom_cursor_out_x=['spec1d+ctrl+9'],
+            kp_zoom_cursor_out_y=['spec1d+shift+('],
 
             kp_zoom_fit=['spec1d+backquote'],
             kp_zoom_fit_x=['spec1d+1'],
@@ -113,11 +125,10 @@ class Spec1DMode(Plot2DMode):
 
             kp_cut_x_lo=['spec1d+k'],
             kp_cut_x_hi=['spec1d+l'],
-            kp_cut_x_range=['spec1d+singlequote'],
             kp_cut_y_lo=['spec1d+K'],
             kp_cut_y_hi=['spec1d+L'],
 
-            kp_unsmooth=['spec1d+A'],
+            kp_no_smooth=['spec1d+A'],
             kp_smooth_less=['spec1d+a'],
             kp_smooth_more=['spec1d+s'],
 
@@ -157,11 +168,11 @@ class Spec1DMode(Plot2DMode):
 
     #####  KEYBOARD ACTION CALLBACKS #####
 
-    def kp_unsmooth(self, viewer, event, data_x, data_y):
+    def kp_no_smooth(self, viewer, event, data_x, data_y):
         event.accept()
 
         plugin = self.get_plugin()
-        plugin.unsmooth()
+        plugin.no_smooth()
 
     def kp_smooth_more(self, viewer, event, data_x, data_y):
         event.accept()
