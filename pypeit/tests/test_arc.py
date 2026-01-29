49e5450de6cbc44a5d3a5875f9030a6542eeeaac
J. Xavier Prochaska <xavier@ucolick.org>
2026-01-29 14:28:02 +0900

    test fix

--- pypeit/tests/test_arc.py
+++ pypeit/tests/test_arc.py
@@ -12,7 +12,7 @@ def test_detect_lines():
     # Using Paranal night sky as an 'arc'
     arx_sky = io.load_sky_spectrum('paranal_sky.fits')
     arx_amp_true, arx_amp, arx_cent, arx_wid, arx_centerr, arx_w, arx_yprep, _ \
-            = arc.detect_lines(arx_sky.flux.value)
+            = arc.detect_lines(arx_sky.flux)
     assert (len(arx_w) > 3275)
 
 # Many more functions in pypeit.core.arc that need tests!
